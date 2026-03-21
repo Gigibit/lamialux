e86f23ee9956a99cd1cfc9f95bc7dd63a8dc1c9c
Luigi Chougrad <37575673+Gigibit@users.noreply.github.com>
2026-03-21 09:07:00 +0100

    Fix local static stylesheet serving

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
+from functools import partial
+
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.staticfiles.views import serve
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("audiobook.urls")),
+    re_path(r"^static/(?P<path>.*)$", partial(serve, insecure=True)),
 ]
